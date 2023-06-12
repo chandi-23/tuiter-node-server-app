3de8aa262104cec3760903fcb2dc5059aff5341b
Chandresh Lokesha <chandresh@Chandreshs-MacBook-Pro.local>
2023-06-12 12:32:44 -0400

    cors-fix

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ import cors from "cors"
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 
-
-
 const app = express()
 
 app.use(
@@ -18,12 +16,27 @@ app.use(
     })
    );
    
-app.use( 
+app.use(
     cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
- )
+      credentials: true,
+      origin: "https://voluble-eclair-87f921.netlify.app",
+      methods: ["GET", "POST","PUT","DELETE"]
+    })
+   );
+   app.use(function (req, res, next) {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "https://voluble-eclair-87f921.netlify.app"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, POST, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 
 
 app.use(express.json())
